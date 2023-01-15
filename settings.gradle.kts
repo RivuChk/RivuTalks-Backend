0a62941eaeb5f5f165fe3b55868ce5183e207f4f
Rivu Chakraborty <rivu.chakraborty6174@gmail.com>
2023-01-15 17:44:41 +0530

    init project

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "rivu-talks-backend"
---
+++ src/main/kotlin/dev/rivu/plugins/HTTP.kt
@@ -0,0 +1,24 @@
+package dev.rivu.plugins
+
+import io.ktor.server.plugins.cachingheaders.*
+import io.ktor.http.content.*
+import io.ktor.http.*
+import io.ktor.server.plugins.conditionalheaders.*
+import io.ktor.server.plugins.swagger.*
+import io.ktor.server.routing.*
+import io.ktor.server.application.*
+
+fun Application.configureHTTP() {
+    install(CachingHeaders) {
+        options { call, outgoingContent ->
+            when (outgoingContent.contentType?.withoutParameters()) {
+                ContentType.Text.CSS -> CachingOptions(CacheControl.MaxAge(maxAgeSeconds = 24 * 60 * 60))
+                else -> null
+            }
+        }
+    }
+    install(ConditionalHeaders)
+    /*routing {
+        swaggerUI(path = "openapi")
+    }*/
+}
