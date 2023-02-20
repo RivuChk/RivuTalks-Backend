33cc54e162724bf016e3ad36768e07efb363b7a1
Rivu Chakraborty <rivu.chakraborty6174@gmail.com>
2023-02-20 20:14:45 +0530

    update about-me

--- src/main/kotlin/dev/rivu/rivutalks/feature/aboutme/AboutmeController.kt
+++ src/main/kotlin/dev/rivu/rivutalks/feature/aboutme/AboutmeController.kt
@@ -12,9 +12,9 @@ class AboutmeController {
         Success(
             AboutMeResponse(
                 headline = "India's first GDE (Google Dev Expert) for Kotlin | Android Architect @ Viacom18 | Ex @ Meesho, Gojek Tech, Paytm Chat, Paytm Insider, Paytm Movies, BYJU'S | Tech Speaker | Author",
-                aboutMeDetails = """Rivu Chakraborty, is an ADHDer (diagnosed) who may be on the Autism (undiagnosed, yet) spectrum as well. He is a community person, an Android & Kotlin developer, one of the early adopters of Kotlin. **India's first GDE (Google Developers Expert) for Kotlin (https://developers.google.com/community/experts/directory/profile/profile-rivu-chakraborty)**.
+                aboutMeDetails = """Rivu Chakraborty, is an ADHDer (diagnosed) who may be on the Autism (undiagnosed, yet) spectrum as well. He is a community person, an Android & Kotlin developer, one of the early adopters of Kotlin. **India's first GDE (Google Developers Expert) for Kotlin [(https://developers.google.com/community/experts/directory/profile/profile-rivu-chakraborty)](https://developers.google.com/community/experts/directory/profile/profile-rivu-chakraborty)**.
 
-Rivu is currently working as **Senior Manager / Android Architect** at **[Viacom18](https://www.viacom18.com/)**
+Rivu is currently working as **Senior Manager / Android Architect** at **[Viacom18](https://www.viacom18.com/)**.
 He has previously worked with many leading startups in India and South-East Asia as listed below.
 - As **SDE-4 / Lead Engineer (Android)** at **[Meesho (FashNear Technologies Pvt. Ltd.)](https://meesho.io/)**. 
 - As **Senior Android Developer - L3** at **[Gojek (GoProducts Engineering India LLP)](https://www.gojek.io/)**.
@@ -27,7 +27,7 @@ Rivu considers himself a Kotlin and Android enthusiast cum evangelist. He has be
 
 Along with organising events, he also speaks at events, conferences and meetups.
 
-##### For more details on him, and to read his blogs, please visit [rivu.dev](https://rivu.dev) and [rivu.life](https://rivu.life)
+### For more details on him, and to read his blogs, please visit [rivu.dev](https://rivu.dev) and [rivu.life](https://rivu.life)
                 """.trimIndent(),
             )
         )
